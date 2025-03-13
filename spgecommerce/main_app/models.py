0c6f6a09b9d6968599f903211e7bf6e787c21551
StephanieStralina <StephanieDRichter@gmail.com>
2025-03-13 12:37:02 -0500

    Add Product class to models.py

--- spgecommerce/main_app/models.py
+++ spgecommerce/main_app/models.py
@@ -16,6 +16,11 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-#class Product(models.Model):
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=500, default='', blank=True, null=True)
+    img = models.ImageField(upload_to='uploads/product/')
 
 #class Order(models.Model):
