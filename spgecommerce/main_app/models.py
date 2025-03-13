e939ad7eb3e14a311d5c423839284f486592e650
StephanieStralina <StephanieDRichter@gmail.com>
2025-03-13 12:29:31 -0500

    Add Customer class to models.py

--- spgecommerce/main_app/models.py
+++ spgecommerce/main_app/models.py
@@ -7,7 +7,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-#class Customer(models.Model):
+class Customer(models.Model):
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    email = models.EmailField(max_length=100)
+    #password = look at doing this with email hash password tokens?
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 #class Product(models.Model):
 
