761f2726efda0f677d6a457e8e69a8aaa64afdb5
StephanieStralina <StephanieDRichter@gmail.com>
2025-03-13 12:24:18 -0500

    Add Category class to models.py

--- spgecommerce/main_app/models.py
+++ spgecommerce/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
+
+#class Customer(models.Model):
+
+#class Product(models.Model):
+
+#class Order(models.Model):
